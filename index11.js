55bbe1cb7a8e5a37f52f165daaca4b0a74b7ed06
Thiago Assis <55933312+thiagoassisk8@users.noreply.github.com>
2020-02-10 07:10:50 -0300

    Add files via upload

--- index11.js
+++ index11.js
@@ -1,19 +1,13 @@
 const puppeteer = require('puppeteer');
 
- async function run(){
+async function run(){
     const browser = await puppeteer.launch({headless:false})
     const page = await browser.newPage()
     await page.goto('https://www.ibge.gov.br/estatisticas/economicas/industria/9294-pesquisa-industrial-mensal-producao-fisica-brasil.html?=&t=resultados');
-    //await new Promise(resolve => setTimeout(resolve, 5000));  
-    var selectedValue = await page.evaluate(() => {
-        return $(`select#tabelasidra20180414509450export.selectExport`)[0].value
-      });
-    await console.log("Selected Value"+ selectedValue)
-
-    var selectedOrNot = await page.evaluate(() => {
-        return $(`select [value='Microsoft']`)[0].selected
-      });
-    await console.log("Selected Or Not "+ selectedOrNot)
+    await new Promise(resolve => setTimeout(resolve, 2500));  
+    await page.select("select#seriehistorica201804145443525export", "csv")
+    //await page.waitFor(3000)
+    //await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './Documentos'});
     await browser.close();
 }
 run()
